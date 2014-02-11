dddc53208c7e262419f360a26cb3243beeb1ff55
albertd <albert@dahan.nl>
2014-02-11 16:35:55 +0100

    pango: bump to version 1.36.2

--- package/pango/pango.mk
+++ package/pango/pango.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PANGO_VERSION_MAJOR = 1.28
-PANGO_VERSION_MINOR = 4
+PANGO_VERSION_MAJOR = 1.36
+PANGO_VERSION_MINOR = 2
 PANGO_VERSION = $(PANGO_VERSION_MAJOR).$(PANGO_VERSION_MINOR)
 
-PANGO_SOURCE = pango-$(PANGO_VERSION).tar.bz2
+PANGO_SOURCE = pango-$(PANGO_VERSION).tar.xz
 PANGO_SITE = http://ftp.gnome.org/pub/GNOME/sources/pango/$(PANGO_VERSION_MAJOR)
 PANGO_AUTORECONF = YES
 PANGO_INSTALL_STAGING = YES
@@ -45,6 +45,7 @@ PANGO_CONF_OPT = --enable-explicit-deps=no --disable-debug
 PANGO_DEPENDENCIES = $(if $(BR2_NEEDS_GETTEXT_IF_LOCALE),gettext) \
 	host-pkgconf \
 	libglib2 \
+	harfbuzz \
 	cairo \
 	fontconfig \
 	freetype
