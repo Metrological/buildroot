ba2631bcc0a2dba8edc7e19c9b743ad42a915631
albertd <albert@dahan.nl>
2014-02-11 21:03:58 +0100

    qt5webkit: adding tools and rpi egl mediaplayer for gstreamer 0.10

--- package/qt5/qt5webkit/qt5webkit.mk
+++ package/qt5/qt5webkit/qt5webkit.mk
@@ -28,12 +28,60 @@ ifeq ($(BR2_PACKAGE_QT5DECLARATIVE),y)
 QT5WEBKIT_DEPENDENCIES += qt5declarative
 endif
 
+ifeq ($(BR2_ENABLE_DEBUG),y)
+	QT5WEBKIT_DEBUG_CONFIG = "CONFIG+=debug"
+else
+	QT5WEBKIT_DEBUG_CONFIG = "CONFIG-=debug"
+endif
+
+ifeq ($(BR2_PACKAGE_MINIBROWSER),y)
+	QT5WEBKIT_POST_BUILD_HOOKS += QT5WEBKIT_BUILD_MINIBROWSER
+endif
+
+ifeq ($(BR2_PACKAGE_TESTBROWSER),y)
+	QT5WEBKIT_POST_BUILD_HOOKS += QT5WEBKIT_BUILD_TESTBROWSER
+endif
+
+ifeq ($(BR2_PACKAGE_DUMPRENDERTREE), y)
+	QT5WEBKIT_POST_BUILD_HOOKS += QT5WEBKIT_BUILD_MINIBROWSER
+endif
+
 define QT5WEBKIT_CONFIGURE_CMDS
-	(cd $(@D); $(TARGET_MAKE_ENV) $(HOST_DIR)/usr/bin/qmake)
+	(cd $(@D); \
+		$(TARGET_MAKE_ENV) \
+		$(HOST_DIR)/usr/bin/qmake \
+			CONFIG+=release \
+			CONFIG+=use_gstreamer010 \
+			$(QT5WEBKIT_DEBUG_CONFIG) \
+	)
 endef
 
 define QT5WEBKIT_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define QT5WEBKIT_BUILD_MINIBROWSER
+	(cd $(@D)/Tools/MiniBrowser/qt; \
+		$(TARGET_MAKE_ENV) \
+		$(HOST_DIR)/usr/bin/qmake \
+	)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/Tools/MiniBrowser/qt
+endef
+
+define QT5WEBKIT_BUILD_TESTBROWSER
+	(cd $(@D)/Tools/QtTestBrowser; \
+		$(TARGET_MAKE_ENV) \
+		$(HOST_DIR)/usr/bin/qmake \
+	)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/Tools/QtTestBrowser
+endef
+
+define QT5WEBKIT_BUILD_DUMPRENDERTREE
+	(cd $(@D)/Tools/DumpRenderTree/qt; \
+		$(TARGET_MAKE_ENV) \
+		$(HOST_DIR)/usr/bin/qmake ./DumpRenderTree.pro \
+	)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/Tools/DumpRenderTree/qt
 endef
 
 define QT5WEBKIT_INSTALL_STAGING_CMDS
@@ -48,6 +96,7 @@ endef
 endif
 
 define QT5WEBKIT_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/root/.cache
 	cp -dpf $(STAGING_DIR)/usr/lib/libQt5WebKit*.so.* $(TARGET_DIR)/usr/lib
 	cp -dpf $(@D)/bin/* $(TARGET_DIR)/usr/bin/
 	$(QT5WEBKIT_INSTALL_TARGET_QMLS)
