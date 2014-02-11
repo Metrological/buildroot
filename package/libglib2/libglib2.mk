75f38d024397095633e15c1a157e84c5e988e18f
albertd <albert@dahan.nl>
2014-02-11 15:46:49 +0100

    libglib2: bump to version 2.39.2

--- package/libglib2/libglib2.mk
+++ package/libglib2/libglib2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBGLIB2_VERSION_MAJOR = 2.38
+LIBGLIB2_VERSION_MAJOR = 2.39
 LIBGLIB2_VERSION_MINOR = 2
 LIBGLIB2_VERSION = $(LIBGLIB2_VERSION_MAJOR).$(LIBGLIB2_VERSION_MINOR)
 LIBGLIB2_SOURCE = glib-$(LIBGLIB2_VERSION).tar.xz
