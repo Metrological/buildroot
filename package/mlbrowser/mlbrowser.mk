f86f754fb4454ab75cbbccc40112f0f0732cb2f7
albertd <albert@dahan.nl>
2014-02-18 22:14:17 +0100

    mlbrowser: cleaning up

--- package/mlbrowser/mlbrowser.mk
+++ package/mlbrowser/mlbrowser.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 MLBROWSER_VERSION = ebf6586ed9f2d38ac0486c2797eaa885d18df303
-MLBROWSER_SITE_METHOD = git
-MLBROWSER_SITE = https://github.com/albertd/mlbrowser.git
+MLBROWSER_SITE = $(call github,albertd,mlbrowser,$(MLBROWSER_VERSION))
 
 ifeq ($(BR2_PACKAGE_QT5WEBKIT),y)
 MLBROWSER_DEPENDENCIES = qt5webkit
