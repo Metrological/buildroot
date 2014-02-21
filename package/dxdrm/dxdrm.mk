3e53a6d06f9829f918e06f5c429c1edd4a736ca3
albertd <albert@dahan.nl>
2014-02-22 00:59:13 +0100

    dxdrm: adding package

---
+++ package/dxdrm/dxdrm.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# dxdrm
+#
+################################################################################
+
+DXDRM_VERSION = 1.5.0
+DXDRM_SITE = ../dxdrm
+DXDRM_SITE_METHOD = local
+DXDRM_LICENSE = PROPRIETARY
+DXDRM_REDISTRIBUTE = NO
+
+DXDRM_INSTALL_STAGING = YES
+
+define DXDRM_INSTALL_STAGING_CMDS
+	$(INSTALL) -m 755 $(@D)/dxdrm.$(call qstrip,$(BR2_ARCH)) $(STAGING_DIR)/usr/lib/libDxDrm.so
+	$(INSTALL) -d -m 755 $(STAGING_DIR)/usr/include/dxdrm
+	$(INSTALL) -m 644 $(@D)/include/*.h $(STAGING_DIR)/usr/include/dxdrm
+endef
+
+define DXDRM_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 $(@D)/dxdrm.$(call qstrip,$(BR2_ARCH)) $(TARGET_DIR)/usr/lib/libDxDrm.so
+	$(INSTALL) -d -m 755 $(TARGET_DIR)/etc/dxdrm
+	$(INSTALL) -m 644 $(@D)/dxdrm.config $(TARGET_DIR)/etc/dxdrm
+	$(INSTALL) -m 644 $(@D)/credentials/* $(TARGET_DIR)/etc/dxdrm
+endef
+
+$(eval $(generic-package))
