8152c0bd937f5c90892610adb8b9991e3199764b
albertd <albert@dahan.nl>
2014-02-22 01:44:47 +0100

    gmrender: adding package

---
+++ package/gmrender/gmrender.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# gmrender
+#
+################################################################################
+
+GMRENDER_VERSION = 45346446a25c353152c8a68ae64bb5deb6c855a3
+GMRENDER_SITE = $(call github,hzeller,gmrender-resurrect,$(GMRENDER_VERSION))
+
+GMRENDER_DEPENDENCIES = host-pkgconf gstreamer1 gst1-plugins-base gst1-omx libupnp
+
+GMRENDER_AUTORECONF = YES
+
+$(eval $(autotools-package))
