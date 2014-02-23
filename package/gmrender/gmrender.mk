32282158e0d85b071d7909d708d760460dcf00e1
albertd <albert@dahan.nl>
2014-02-23 13:28:42 +0100

    gmrender: fix typo in dependencies

--- package/gmrender/gmrender.mk
+++ package/gmrender/gmrender.mk
@@ -7,7 +7,7 @@
 GMRENDER_VERSION = 45346446a25c353152c8a68ae64bb5deb6c855a3
 GMRENDER_SITE = $(call github,hzeller,gmrender-resurrect,$(GMRENDER_VERSION))
 
-GMRENDER_DEPENDENCIES = host-pkgconf ibupnp gstreamer1 gst1-plugins-base
+GMRENDER_DEPENDENCIES = host-pkgconf libupnp gstreamer1 gst1-plugins-base
 
 GMRENDER_AUTORECONF = YES
 
