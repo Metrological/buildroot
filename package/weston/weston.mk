f2878113ba8b1bfa7af37c2f39638aa213c84fd0
Zan Dobersek <zdobersek@igalia.com>
2014-02-24 22:07:29 +0100

    Enable EGL clients for Weston when building the RPi backend
    The (simple) EGL clients should be enabled when building the RPi backend of Weston.
    They should also be installable so they can be tested out on target hardware.
    weston-launch is also enabled and the Cairo image backend is explicitly demanded.

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -18,30 +18,34 @@ WESTON_AUTORECONF = YES
 
 WESTON_CONF_OPT = \
 	--with-dtddir=$(STAGING_DIR)/usr/share/wayland \
-	--disable-egl \
-	--disable-simple-egl-clients \
 	--disable-xwayland \
+	--disable-xwayland-test \
 	--disable-x11-compositor \
 	--disable-drm-compositor \
 	--disable-wayland-compositor \
 	--disable-headless-compositor \
-	--disable-weston-launch \
 	--disable-colord \
 	--disable-libunwind
 
 ifeq ($(BR2_PACKAGE_WESTON_FBDEV),y)
-WESTON_CONF_OPT += --enable-fbdev-compositor
+WESTON_CONF_OPT += \
+	--enable-fbdev-compositor \
+	--disable-egl \
+	--disable-simple-egl-clients
 else
 WESTON_CONF_OPT += --disable-fbdev-compositor
 endif
 
 ifeq ($(BR2_PACKAGE_WESTON_RPI),y)
 WESTON_DEPENDENCIES += rpi-userland
-WESTON_CONF_OPT += --enable-rpi-compositor \
+WESTON_CONF_OPT += \
+	--enable-rpi-compositor \
+	--enable-weston-launch \
+	--with-cairo=image \
 	--disable-resize-optimization \
 	--disable-setuid-install \
-	--disable-xwayland-test \
-	--disable-simple-egl-clients \
+	--disable-setuid-install \
+	--enable-demo-clients-install \
 	WESTON_NATIVE_BACKEND=rpi-backend.so
 else
 WESTON_CONF_OPT += --disable-rpi-compositor
