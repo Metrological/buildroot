011ed65998f0fe50d20367609347dbdc384f8b9a
albertd <albert@dahan.nl>
2014-03-04 20:51:10 +0100

    rpi-firmware: adding option to add wlan0 interface

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -9,6 +9,13 @@ RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
 
+ifeq ($(BR2_PACKAGE_RPI_FIRMWARE_WIFI),y)
+define RPI_FIRMWARE_INSTALL_TARGET_WIFI
+	grep -q 'wlan0' $(TARGET_DIR)/etc/network/interfaces || \
+		echo -e '\nauto wlan0\niface wlan0 inet dhcp\nwpa-roam /etc/wpa_supplicant.conf\npre-up wpa_supplicant -c /etc/wpa_supplicant.conf -i wlan0 -B -P /var/run/wpa_supplicant -dd\npre-down killall wpa_supplicant' >> $(TARGET_DIR)/etc/network/interfaces
+endef
+endif
+
 define RPI_FIRMWARE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/boot/bootcode.bin $(BINARIES_DIR)/rpi-firmware/bootcode.bin
 	$(INSTALL) -D -m 0644 $(@D)/boot/start$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).elf $(BINARIES_DIR)/rpi-firmware/start.elf
@@ -17,6 +24,7 @@ define RPI_FIRMWARE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 package/rpi-firmware/cmdline.txt $(BINARIES_DIR)/rpi-firmware/cmdline.txt
 	grep -q 'eth0' $(TARGET_DIR)/etc/network/interfaces || \
 		echo -e 'auto eth0\niface eth0 inet dhcp' >> $(TARGET_DIR)/etc/network/interfaces
+	$(RPI_FIRMWARE_INSTALL_TARGET_WIFI)
 	mkdir -p $(TARGET_DIR)/boot
 	grep -q '^/dev/mmcblk0p1' $(TARGET_DIR)/etc/fstab || \
 		echo -e '/dev/mmcblk0p1 /boot vfat defaults 0 0' >> $(TARGET_DIR)/etc/fstab
