4f0bf2f03b90c98fde9ad9ee8312108da1d27c8f
albertd <albert@dahan.nl>
2014-03-07 20:28:58 +0100

    gst-flucodecpack: cleaning up

--- package/gstreamer1/gst1-flucodecpack/gst1-flucodecpack.mk
+++ package/gstreamer1/gst1-flucodecpack/gst1-flucodecpack.mk
@@ -14,14 +14,14 @@ GST1_FLUCODECPACK_DEPENDENCIES += libcurl
 
 define GST1_FLUCODECPACK_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/gstreamer-1.0; \
-	$(INSTALL) -m 755 $(@D)/fluaacdec.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfluaacdec.so; \
-	$(INSTALL) -m 755 $(@D)/fluac3dec.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfluac3dec.so; \
-	$(INSTALL) -m 755 $(@D)/fluadpcm.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfluadpcm.so; \
-	$(INSTALL) -m 755 $(@D)/fluasfdemux.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfluasfdemux.so; \
-	$(INSTALL) -m 755 $(@D)/fluilbc.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfluilbc.so; \
-	$(INSTALL) -m 755 $(@D)/flump3dec.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstflump3dec.so; \
-	$(INSTALL) -m 755 $(@D)/flussdemux.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstflussdemux.so; \
-	$(INSTALL) -m 755 $(@D)/fluwmadec.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfluwmadec.so
+	$(INSTALL) -m 755 $(@D)/fluaacdec.bin $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstfluaacdec.so; \
+	$(INSTALL) -m 755 $(@D)/fluac3dec.bin $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstfluac3dec.so; \
+	$(INSTALL) -m 755 $(@D)/fluadpcm.bin $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstfluadpcm.so; \
+	$(INSTALL) -m 755 $(@D)/fluasfdemux.bin $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstfluasfdemux.so; \
+	$(INSTALL) -m 755 $(@D)/fluilbc.bin $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstfluilbc.so; \
+	$(INSTALL) -m 755 $(@D)/flump3dec.bin $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstflump3dec.so; \
+	$(INSTALL) -m 755 $(@D)/flussdemux.bin $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstflussdemux.so; \
+	$(INSTALL) -m 755 $(@D)/fluwmadec.bin $(TARGET_DIR)/usr/lib/gstreamer-1.0/libgstfluwmadec.so
 endef
 
 $(eval $(generic-package))
