8dbff25dafc092bbd7a513a3c2f53e2e78b4eb5c
msieben <m.sieben@metrological.com>
2014-03-13 14:19:11 +0100

    apitrace: fix build for non-x11 unix platforms
    egl-types for unix platforms do not have to depend solely on x11-types

--- package/apitrace/apitrace.mk
+++ package/apitrace/apitrace.mk
@@ -9,7 +9,11 @@ APITRACE_SITE = $(call github,apitrace,apitrace,$(APITRACE_VERSION))
 APITRACE_LICENSE = MIT
 APITRACE_LICENSE_FILES = LICENSE
 
-APITRACE_DEPENDENCIES = xlib_libX11 host-python
+APITRACE_DEPENDENCIES = host-python
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBX11), y)
+APITRACE_DEPENDENCIES += xlib_libX11
+endif
 
 # Gui was never tested, so we prefer to explicitly disable it
 APITRACE_CONF_OPT += -DENABLE_GUI=false
