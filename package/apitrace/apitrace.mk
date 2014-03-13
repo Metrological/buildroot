11b72d70d53cfda436f1411f94cff2ee436b361b
albertd <albert@dahan.nl>
2014-03-13 16:18:26 +0100

    apitrace: bump version

--- package/apitrace/apitrace.mk
+++ package/apitrace/apitrace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-APITRACE_VERSION = c181b7fbf4b1c3912424761fec8ac7124640543a
+APITRACE_VERSION = 485846b2a40d8ac7d6c1c5f8af6d15b0c10af19d
 APITRACE_SITE = $(call github,apitrace,apitrace,$(APITRACE_VERSION))
 APITRACE_LICENSE = MIT
 APITRACE_LICENSE_FILES = LICENSE
