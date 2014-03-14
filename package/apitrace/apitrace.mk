8c4957e00aed791a2a7fd9b83bf6c6767df0a7f2
msieben <m.sieben@metrological.com>
2014-03-14 16:41:21 +0100

    apitrace: enable eglretrace build
    eglretrace is required by apitrace to support certain functionality

--- package/apitrace/apitrace.mk
+++ package/apitrace/apitrace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-APITRACE_VERSION = 485846b2a40d8ac7d6c1c5f8af6d15b0c10af19d
+APITRACE_VERSION = 084fe926e8fbbdf41f19d844157e4baac6a6b538
 APITRACE_SITE = $(call github,apitrace,apitrace,$(APITRACE_VERSION))
 APITRACE_LICENSE = MIT
 APITRACE_LICENSE_FILES = LICENSE
@@ -13,6 +13,11 @@ APITRACE_DEPENDENCIES = host-python
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBX11), y)
 APITRACE_DEPENDENCIES += xlib_libX11
+else
+APITRACE_CONF_OPT += \
+	-DSYSTEM_EGL=false \
+	-DENABLE_EGL_NO_X11=true \
+	-DCMAKE_CPP_FLAGS="$(TARGET_CPPFLAGS) -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads" 
 endif
 
 # Gui was never tested, so we prefer to explicitly disable it
