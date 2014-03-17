7efb667f27fb7697b3b73946c0aae37918ad1a1d
msieben <m.sieben@metrological.com>
2014-03-17 17:39:53 +0100

    apitrace: bump version and enable build for rpi
    the target requires a platform initialisation; a platform initialisation for the raspberry has been added

--- package/apitrace/apitrace.mk
+++ package/apitrace/apitrace.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-APITRACE_VERSION = 084fe926e8fbbdf41f19d844157e4baac6a6b538
+APITRACE_VERSION = 485846b2a40d8ac7d6c1c5f8af6d15b0c10af19d
 APITRACE_SITE = $(call github,apitrace,apitrace,$(APITRACE_VERSION))
 APITRACE_LICENSE = MIT
 APITRACE_LICENSE_FILES = LICENSE
@@ -13,11 +13,24 @@ APITRACE_DEPENDENCIES = host-python
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBX11), y)
 APITRACE_DEPENDENCIES += xlib_libX11
-else
+endif
+
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
+APITRACE_CFLAGS += -DPLATFORM_RPI
+APITRACE_CXXFLAGS += -DPLATFORM_RPI
+endif
+
+ifeq ($(findstring y,$(BR2_PACKAGE_DAWN_SDK)$(BR2_PACKAGE_BCM_REFSW)),y)
+#These platforms are currently not supported
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_OPENGL_EGL), y)
 APITRACE_CONF_OPT += \
 	-DSYSTEM_EGL=false \
 	-DENABLE_EGL_NO_X11=true \
-	-DCMAKE_CPP_FLAGS="$(TARGET_CPPFLAGS) -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads" 
+	-DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) $(APITRACE_CXXFLAGS) $(shell $(PKG_CONFIG_HOST_BINARY) --cflags egl)" \
+	-DCMAKE_C_FLAGS="$(TARGET_CFLAGS) $(APITRACE_CFLAGS) $(shell $(PKG_CONFIG_HOST_BINARY) --cflags egl)" \
+	-DCMAKE_EXE_LINKER_FLAGS="$(TARGET_LDFLAGS) $(APITRACE_LDFLAGS) $(shell $(PKG_CONFIG_HOST_BINARY) --libs egl)"
 endif
 
 # Gui was never tested, so we prefer to explicitly disable it
