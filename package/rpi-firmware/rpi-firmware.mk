492821456e40cc67c611559174c3069ea12e2ffa
albertd <albert@dahan.nl>
2014-03-25 20:30:04 +0100

    rpi-firmware: bump to latest version

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 18a79214087242e683911e2bb6b85deb98e32baa
+RPI_FIRMWARE_VERSION = 9953ce0847c04b587b694c79c5b5a4ac63f5c369
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
