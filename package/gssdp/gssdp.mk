7efbe492356f962830ede2aba3f152a9a79cbfc0
albertd <albert@dahan.nl>
2014-04-17 13:52:37 +0200

    gssdp: cleaning up package

--- package/gssdp/gssdp.mk
+++ package/gssdp/gssdp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-GSSDP_VERSION:=0.14.7
-GSSDP_SOURCE:=gssdp-$(GSSDP_VERSION).tar.xz
-GSSDP_SITE:=http://ftp.gnome.org/pub/GNOME/sources/gssdp/0.14/
+GSSDP_VERSION = 0.14.7
+GSSDP_SOURCE = gssdp-$(GSSDP_VERSION).tar.xz
+GSSDP_SITE = http://ftp.gnome.org/pub/GNOME/sources/gssdp/0.14/
 GSSDP_INSTALL_STAGING = YES
 GSSDP_INSTALL_TARGET = YES
 
