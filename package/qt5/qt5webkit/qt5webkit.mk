3b17248ff37a180f96d8ed04d5420e2ea6de6961
albertd <albert@dahan.nl>
2014-04-29 10:34:36 +0200

    qt5webkit: bump to latest version adding nsd support
    4b36814 NSD: [Qt][WK1] HACK: unconditionally allow access to services
    c647bb8 NSD: [Qt][WK1] Implement ::notifyDiscoveryFinished()
    5c0e4fb NSD: [Qt][WK1] basic NetworkServicesClient
    a68bc6c NSD: general build fixes
    adb7f46 NSD: Qt build fixes
    f11bc39 NSD: Qt build support
    817aebd NSD: Update WebKit Build to compile NSD
    6971a89 NSD: Adding discovery providers (UPnP and ZeroConf)
    4e63da2 NSD: Base implementation of NSD within WebCore
    7692ee9 Revert "Add basic discovery API."

--- package/qt5/qt5webkit/qt5webkit.mk
+++ package/qt5/qt5webkit/qt5webkit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QT5WEBKIT_VERSION = ca2f6a9b0ffdd92a843b0b173abea4dc060ef22c
+QT5WEBKIT_VERSION = 4b3681455c1f3155ea35fd7a006201566559c88e
 QT5WEBKIT_SITE = $(call github,Metrological,qtwebkit,$(QT5WEBKIT_VERSION))
 QT5WEBKIT_DEPENDENCIES = qt5base sqlite host-ruby host-gperf host-bison host-flex
 QT5WEBKIT_INSTALL_STAGING = YES
