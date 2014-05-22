0291fbcfb38e147158eeaf880fce53f1a65283ed
Philippe Normand <philn@igalia.com>
2014-05-22 09:37:46 +0200

    qt5webkit: bump for WebKit2 video support
    15d0860 Recreate GraphicsSurface when video size changes.
    efee50a [GStreamer] Reuse video texture
    db0111c set GraphicsSurface y-flip flag according to EGLImage orientation
    89dc7ea Add flag to select whether the GraphicsSurface should flip the texture
    193f725 Zero-copy video texture rendering
    8447a27 Fix y-flip video texture support
    4829f91 remove obsolete 0.10 code
    3b8c8ce Fix unsafe memory load/store from the argument encoder/decoder affecting ARM https://bugs.webkit.org/show_bug.cgi?id=125674
    caa6b83 [GraphicsSurface] Remove unneeded transform when painting to Texture Mapper
    5a0523c Do not flip image when painting from GraphicsSurface

--- package/qt5/qt5webkit/qt5webkit.mk
+++ package/qt5/qt5webkit/qt5webkit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QT5WEBKIT_VERSION = 19b20a6b5577eb9dee843c969ffe106584df53b7
+QT5WEBKIT_VERSION = 15d086052f3096d09f7ba65ab306769064719ba7
 QT5WEBKIT_SITE = $(call github,Metrological,qtwebkit,$(QT5WEBKIT_VERSION))
 
 QT5WEBKIT_DEPENDENCIES = qt5base sqlite host-ruby host-gperf host-bison host-flex
