e9b1ffd4be8c3de58231cbd1f6c525601a906e85
Guillaume Emont <guijemont@igalia.com>
2014-05-27 14:35:30 +0200

    Fix qtwebkit commit to get.
    The commit pointed to did not exist. Fixing it to point to the intended commit.

--- package/qt5/qt5webkit/qt5webkit.mk
+++ package/qt5/qt5webkit/qt5webkit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QT5WEBKIT_VERSION = 5f36e1d12f5e851b474525bcb9ba74706ac5d538
+QT5WEBKIT_VERSION = 08ea9e6da358b3688e803c1a22041b52de6692ae
 QT5WEBKIT_SITE = $(call github,Metrological,qtwebkit,$(QT5WEBKIT_VERSION))
 QT5WEBKIT_DEPENDENCIES = qt5base sqlite host-ruby host-gperf host-bison host-flex
 QT5WEBKIT_INSTALL_STAGING = YES
