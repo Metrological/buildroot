0eb11cff702fbad3580accdfebec4078dc4a11c3
msieben <m.sieben@metrological.com>
2014-08-20 13:49:32 +0200

    qtbrowser: bump to version 2.0.1

--- package/qtbrowser/qtbrowser.mk
+++ package/qtbrowser/qtbrowser.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QTBROWSER_VERSION = 2.0.0
+QTBROWSER_VERSION = 2.0.1
 QTBROWSER_SITE = $(call github,metrological,qtbrowser,$(QTBROWSER_VERSION))
 QTBROWSER_LICENSE = GPLv2
 
