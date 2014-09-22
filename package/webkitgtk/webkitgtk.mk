b7c2eac0a16450bac26daaf1bd924323305528cc
Philippe Normand <philn@igalia.com>
2014-09-22 12:36:53 +0200

    webkitgtk: proper Debug/Release build parameters

--- package/webkitgtk/webkitgtk.mk
+++ package/webkitgtk/webkitgtk.mk
@@ -12,10 +12,19 @@ WEBKITGTK_DEPENDENCIES = host-flex host-bison host-gperf host-ruby \
 	icu libxml2 libxslt libgtk3 sqlite enchant libsoup jpeg webp \
 	gstreamer1 gst1-plugins-base gst1-plugins-good gst1-plugins-bad
 
+ifeq ($(BR2_ENABLE_DEBUG),y)
+BUILDTYPE=Debug
+FLAGS= -DCMAKE_C_FLAGS_DEBUG="-O0 -g" \
+ -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g"
+else
+BUILDTYPE=Release
+FLAGS= -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG" \
+ -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG"
+endif
+
 WEBKITGTK_CONF_OPT = -DPORT=GTK \
- -DCMAKE_BUILD_TYPE=Release \
- -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG" \
- -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG" \
+ -DCMAKE_BUILD_TYPE=$(BUILDTYPE) \
+ $(FLAGS) \
  -DENABLE_X11_TARGET=OFF \
  -DENABLE_WAYLAND_TARGET=ON \
  -DENABLE_CREDENTIAL_STORAGE=OFF
