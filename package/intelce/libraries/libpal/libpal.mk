be60a482b30fc035231d1cd60526894c856f3ef5
msieben <m.sieben@metrological.com>
2014-11-10 16:39:18 +0100

    package/intelce/libraries/libpal: initial commit
    Intel CE PAL library

---
+++ package/intelce/libraries/libpal/libpal.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# libpal
+#
+################################################################################
+
+LIBPAL_VERSION = $(INTELCE_VERSION).0
+LIBPAL_SITE = $(INTELCE_SITE)/libpal/
+LIBPAL_SITE_METHOD = local
+LIBPAL_LICENSE = PROPRIETARY
+LIBPAL_REDISTRIBUTE = NO
+
+LIBPAL_DEPENDENCIES = libosal
+
+LIBPAL_INSTALL_STAGING = YES
+
+LIBPAL_CFLAGS = \
+	$(TARGET_CFLAGS) \
+	-I$(@D)/src/include/ \
+	-I$(INTELCE_SITE)/libsven/src/include/ \
+	-I$(STAGING_DIR)/usr/include/intelce/libosal/
+
+define LIBPAL_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) CC="$(TARGET_CC)" CPPFLAGS="$(TARGET_CPPFLAGS)" CFLAGS="$(LIBPAL_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" AR="$(TARGET_CROSS)ar" RANLIB="$(TARGET_CROSS)ranlib" -C $(@D)/src/
+endef
+
+define LIBPAL_INSTALL_STAGING_CMDS
+	mkdir -p $(STAGING_DIR)/usr/lib/intelce/
+	$(INSTALL) -m 755 $(@D)/src/{*.a,*.so} $(STAGING_DIR)/usr/lib/intelce/
+	mkdir -p $(STAGING_DIR)/usr/include/intelce/libpal/
+	$(INSTALL) -m 755 $(@D)/src/include/*.h $(STAGING_DIR)/usr/include/intelce/libpal/
+endef
+
+define LIBPAL_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/intelce/
+	$(INSTALL) -m 755 $(@D)/src/*.so $(TARGET_DIR)/usr/lib/intelce/
+endef
+
+$(eval $(generic-package))
