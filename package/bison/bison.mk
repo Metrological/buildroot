3a0d8312a86bc649cf30c1842408acad63d97102
msieben <m.sieben@metrological.com>
2014-12-02 11:31:41 +0100

    bison: add support for 2.4.3
    The Intel CE Khronos package builds against older versions.

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -4,9 +4,18 @@
 #
 ################################################################################
 
+ifeq ($(BR2_PACKAGE_INTELCE),y)
+BISON_VERSION = 2.4.3
+BISON_SOURCE = bison-$(BISON_VERSION).tar.gz
+else
 BISON_VERSION = 3.0.2
 BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
+endif
+
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
+
+#http://ftp.gnu.org/gnu/bison/
+
 BISON_LICENSE = GPLv3+
 BISON_LICENSE_FILES = COPYING
 BISON_CONF_ENV = ac_cv_path_M4=/usr/bin/m4
