b5d08d596201ce7f8b6b32f4503a852ddf8cc652
msieben <m.sieben@metrological.com>
2014-12-05 16:11:23 +0100

    qt5base: prepare for Intel CE
    Add (empty) platform definition and hook files

--- package/qt5/qt5base/qt5base.mk
+++ package/qt5/qt5base/qt5base.mk
@@ -152,6 +152,10 @@ ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 QT5BASE_EGLFS_PLATFORM_HOOKS_SOURCES = \
 	$(@D)/mkspecs/devices/linux-rasp-pi-g++/qeglfshooks_pi.cpp
 endif
+ifeq ($(BR2_PACKAGE_KHRONOS),y)
+QT5BASE_EGLFS_PLATFORM_HOOKS_SOURCES = \
+	$(@D)/mkspecs/devices/linux-intelce-g++/qeglfshooks_intelce_3100.cpp
+endif
 else
 QT5BASE_CONFIGURE_OPTS += -no-opengl -no-eglfs
 endif
