5eedb68fdd7e15c3648be3f5985b9946b9b959fa
Zan Dobersek <zdobersek@igalia.com>
2015-01-12 02:30:42 -0800

    [weston] Bump to a 1.6-based version.

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WESTON_VERSION = 920a0331cf5340942a9d39ac3b48ce207f7970b6
+WESTON_VERSION = 35811b82c1999ee9544399c60db02b6c1999fcaf
 WESTON_SITE = $(call github,WebKitForWayland,weston,$(WESTON_VERSION))
 WESTON_LICENSE = MIT
 WESTON_LICENSE_FILES = COPYING
