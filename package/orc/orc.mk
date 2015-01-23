dac3de7be0db45ad92b0bb820aa569a42b9ed571
Philippe Normand <philn@igalia.com>
2015-01-23 16:48:51 +0100

    orc: bump to 0.4.22
    0.4.23 doesn't build... Also the code moved to GStreamer freedesktop
    infrastructure.

--- package/orc/orc.mk
+++ package/orc/orc.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-ORC_VERSION = 0.4.18
-ORC_SITE = http://code.entropywave.com/download/orc/
+ORC_VERSION = 0.4.22
+ORC_SOURCE = orc-$(ORC_VERSION).tar.xz
+ORC_SITE = http://gstreamer.freedesktop.org/src/orc/
 ORC_LICENSE = BSD-2c, BSD-3c
 ORC_LICENSE_FILES = COPYING
 ORC_INSTALL_STAGING = YES
