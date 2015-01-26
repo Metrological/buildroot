664bb739f855a57a5d1c3458ab141da5ec743b8a
Zan Dobersek <zdobersek@igalia.com>
2015-01-26 07:49:51 -0800

    [athol] Bump to a version that fixes buffer swapping.

--- package/athol/athol.mk
+++ package/athol/athol.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ATHOL_VERSION = 45f4bb4c133b8e302636735e4e93527efec4f250
+ATHOL_VERSION = 1aa597990216e2f2a8e111d2056d21a8ea2f9b76
 ATHOL_SITE = $(call github,WebKitForWayland,athol,$(ATHOL_VERSION))
 
 ATHOL_INSTALL_STAGING = YES
