bcbb4cdbed9e375309afe0486b63333c92528a8d
Pierre Wielders <pierre@wielders.net>
2015-01-30 19:11:57 +0100

    Building succeeded up to 40% :-) Check in in case the SSD fails again.

--- package/nrd/nrd.mk
+++ package/nrd/nrd.mk
@@ -7,9 +7,12 @@
 NRD_VERSION = master
 NRD_SITE_METHOD = git
 NRD_SITE = git@github.com:Metrological/nrd.git
-NRD_DEPENDENCIES = alsa-lib libjpeg libpng libmng freetype webp
+NRD_DEPENDENCIES = alsa-lib libjpeg libpng libmng freetype webp expat openssl c-ares
 
 NRD_INSTALL_STAGING = YES
+
+NRD_LIBC_VERSION = "-D__UCLIBC__MAJOR=9 -D__UCLIBC_MINOR__=33 -D__UCLIBC_SUBLEVEL__=2"
+#NRD_LIBC_VERSION = "-D__GLIBC__=1 -D__GLIBC_MINOR__=3"
 
 ifeq ($(BR2_ENABLE_DEBUG),y)
 BUILDTYPE=Debug
@@ -28,8 +31,9 @@ NRD_CONFIGURE_CMDS = 		\
 	mkdir $(@D)/output;	\
 	cd $(@D)/output;	\
 	VERBOSE=1 cmake $(@D)/netflix  \
-		-DCMAKE_VERBOSE_MAKEFILE=ON \
 		-DCMAKE_TOOLCHAIN_FILE=$(HOST_DIR)/usr/share/buildroot/toolchainfile.cmake	\
 		-DSMALL_FLAGS:STRING="-s -O3" -DSMALL_CFLAGS:STRING="" -DSMALL_CXXFLAGS:STRING="-fvisibility=hidden -fvisibility-inlines-hidden" -DNRDAPP_TOOLS="manufSSgenerator" -DDPI_REFERENCE_DRM="none"
+
+NRD_BUILD_CMDS = cd $(@D)/output ; make 
 
 $(eval $(cmake-package))
