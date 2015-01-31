5730877201ee82d64e0e0108bba86903c5ed1a8e
Pierre Wielders <pierre@wielders.net>
2015-01-31 12:49:19 +0100

    Building up to 50% :-) Adding some new patches to get nrd SDK up and running. Added several dependencies.

--- package/nrd/nrd.mk
+++ package/nrd/nrd.mk
@@ -7,7 +7,7 @@
 NRD_VERSION = master
 NRD_SITE_METHOD = git
 NRD_SITE = git@github.com:Metrological/nrd.git
-NRD_DEPENDENCIES = alsa-lib libjpeg libpng libmng freetype webp expat openssl c-ares
+NRD_DEPENDENCIES = alsa-lib libjpeg libpng libmng freetype webp expat openssl c-ares portaudio directfb
 
 NRD_INSTALL_STAGING = YES
 
