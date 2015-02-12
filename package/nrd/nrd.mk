f88678df8a98e486c3323a462f4c655c1a512ccd
Pierre Wielders <pierre@wielders.net>
2015-02-12 19:00:59 +0100

    Allow for graphics driver t be selected for the nrd application.

--- package/nrd/nrd.mk
+++ package/nrd/nrd.mk
@@ -16,22 +16,30 @@ NRD_LIBC_VERSION = "-D__UCLIBC__MAJOR=9 -D__UCLIBC_MINOR__=33 -D__UCLIBC_SUBLEVE
 
 ifeq ($(BR2_ENABLE_DEBUG),y)
 BUILDTYPE=Debug
-FLAGS= -DCMAKE_C_FLAGS_DEBUG="-O0 -g -Wno-cast-align" \
- -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g -Wno-cast-align"
+  FLAGS= -DCMAKE_C_FLAGS_DEBUG="-O0 -g -Wno-cast-align" -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g -Wno-cast-align"
 else
 BUILDTYPE=Release
-FLAGS= -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align" \
- -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align"
+  FLAGS= -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align" -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align"
 endif
 
-NRD_CONF_OPT = -DCMAKE_BUILD_TYPE=$(BUILDTYPE) \
- $(FLAGS)
+NRD_CMAKE_FLAGS = -DCMAKE_BUILD_TYPE=$(BUILDTYPE) $(FLAGS)
+
+ifeq ($(BR2_NRD_GRAPHICS_DIRECTFB),y)
+NRD_CMAKE_FLAGS="$(NRD_CMAKE_FLAGS) -DGIBBON_GRAPHICS=\"directfb\"
+else ifeq ($(BR2_NRD_GRAPHICS_GLES2),y)
+NRD_CMAKE_FLAGS="$(NRD_CMAKE_FLAGS) -DGIBBON_GRAPHICS=\"gles2\"
+else ifeq ($(BR2_NRD_GRAPHICS_GLES2_EGL),y)
+NRD_CMAKE_FLAGS="$(NRD_CMAKE_FLAGS) -DGIBBON_GRAPHICS=\"gles2-egl\"
+else 
+NRD_CMAKE_FLAGS="$(NRD_CMAKE_FLAGS) -DGIBBON_GRAPHICS=\"null\"
+endif
 
 NRD_CONFIGURE_CMDS = 		\
 	mkdir $(@D)/output;	\
 	cd $(@D)/output;	\
 	VERBOSE=1 cmake $(@D)/netflix  \
 		-DCMAKE_TOOLCHAIN_FILE=$(HOST_DIR)/usr/share/buildroot/toolchainfile.cmake	\
+		-DCMAKE_FLAGS="$(NRD_CMAKE_FLAGS)						\
 		-DSMALL_FLAGS:STRING="-s -O3" -DSMALL_CFLAGS:STRING="" -DSMALL_CXXFLAGS:STRING="-fvisibility=hidden -fvisibility-inlines-hidden" -DNRDAPP_TOOLS="manufSSgenerator" -DDPI_REFERENCE_DRM="none"
 
 NRD_BUILD_CMDS = cd $(@D)/output ; make 
