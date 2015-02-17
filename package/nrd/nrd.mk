0eb7cda2598e14c4993a75bbb82e104b6aae86c4
Pierre Wielders <pierre@wielders.net>
2015-02-17 11:47:51 +0100

    Make sure the right objcopy is used. The one from the buildroot and not the system one.

--- package/nrd/nrd.mk
+++ package/nrd/nrd.mk
@@ -16,29 +16,33 @@ endif
 NRD_INSTALL_STAGING = YES
 
 ifeq ($(BR2_ENABLE_DEBUG),y)
-  FLAGS= -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-O0 -g -Wno-cast-align" -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g -Wno-cast-align"
+  NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-O0 -g -Wno-cast-align" -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g -Wno-cast-align"
 else
-  FLAGS= -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align" -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align"
+  NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align" -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align"
 endif
 
 ifeq ($(BR2_NRD_GRAPHICS_DIRECTFB),y)
-NRD_CMAKE_FLAGS=$(FLAGS) -DGIBBON_GRAPHICS=directfb
+NRD_GRAPHICS=directfb
 else ifeq ($(BR2_NRD_GRAPHICS_GLES2),y)
-NRD_CMAKE_FLAGS=$(FLAGS) -DGIBBON_GRAPHICS=gles2
+NRD_GRAPHICS=gles2
 else ifeq ($(BR2_NRD_GRAPHICS_GLES2_EGL),y)
-NRD_CMAKE_FLAGS=$(FLAGS) -DGIBBON_GRAPHICS=gles2-egl
+NRD_GRAPHICS=gles2-egl
 else 
-NRD_CMAKE_FLAGS="$(FLAGS) -DGIBBON_GRAPHICS=null"
+NRD_GRAPHICS=null
 endif
+
+NRD_INPUT=devinput
 
 NRD_CONFIGURE_CMDS = 		\
 	mkdir $(@D)/output;	\
 	cd $(@D)/output;	\
-	VERBOSE=1 cmake $(@D)/netflix  \
+	$(TARGET_MAKE_ENV) BUILDROOT_TOOL_PREFIX="$(GNU_TARGET_NAME)-" cmake $(@D)/netflix	\
 		-DCMAKE_TOOLCHAIN_FILE=$(HOST_DIR)/usr/share/buildroot/toolchainfile.cmake	\
-		-DCMAKE_FLAGS="$(NRD_CMAKE_FLAGS)"						\
+		$(NRD_CMAKE_FLAGS)								\
+                -DGIBBON_GRAPHICS=$(NRD_GRAPHICS)						\
+		-DGIBBON_INPUT=$(NRD_INPUT)							\
 		-DSMALL_FLAGS:STRING="-s -O3" -DSMALL_CFLAGS:STRING="" -DSMALL_CXXFLAGS:STRING="-fvisibility=hidden -fvisibility-inlines-hidden" -DNRDAPP_TOOLS="manufSSgenerator" -DDPI_REFERENCE_DRM="none"
 
-NRD_BUILD_CMDS = cd $(@D)/output ; make 
+NRD_BUILD_CMDS = cd $(@D)/output ; $(TARGET_MAKE_ENV) make 
 
 $(eval $(cmake-package))
