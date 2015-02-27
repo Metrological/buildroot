f969f6307e6635573f15bce350e9554ba16a333e
albertd <albert@dahan.nl>
2015-02-27 19:54:12 +0100

    athol: use our fork

--- package/athol/athol.mk
+++ package/athol/athol.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ATHOL_VERSION = bda02c3915d2994b4cc4374b3aa5ae3719ad91f4
-ATHOL_SITE = $(call github,WebKitForWayland,athol,$(ATHOL_VERSION))
+ATHOL_SITE = $(call github,Metrological,athol,$(ATHOL_VERSION))
 
 ATHOL_INSTALL_STAGING = YES
 ATHOL_DEPENDENCIES = wayland libegl libglib2 libinput
