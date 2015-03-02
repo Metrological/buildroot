f9329eb9047412cf87e0cd85d27cab9335fb7009
Zan Dobersek <zdobersek@igalia.com>
2015-03-02 10:04:07 -0800

    Bump to the latest version.

--- package/wpe/wpe.mk
+++ package/wpe/wpe.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPE_VERSION = c0101540b2b8ff31e3a90f87fe714cecaa668bb4
+WPE_VERSION = c2b18cc1cc19322d7192246d36902378d8f2a49c
 WPE_SITE = $(call github,Metrological,WebKitForWayland,$(WPE_VERSION))
 
 WPE_INSTALL_STAGING = YES
