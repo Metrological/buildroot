62b920dca43311052a73c462b0c15bab81c910b9
Pierre Wielders <pierre@wielders.net>
2015-03-02 20:22:46 +0100

    Completed first testrun on the nrd app for the RPI.

--- package/nrd/nrd.mk
+++ package/nrd/nrd.mk
@@ -7,7 +7,7 @@
 NRD_VERSION = master
 NRD_SITE_METHOD = git
 NRD_SITE = git@github.com:Metrological/nrd.git
-NRD_DEPENDENCIES = alsa-lib libjpeg libpng libmng freetype webp expat openssl c-ares portaudio directfb ffmpeg tremor
+NRD_DEPENDENCIES = alsa-lib libjpeg libpng libmng freetype webp expat openssl c-ares portaudio ffmpeg tremor
 
 ifeq ($(BR2_PACKAGE_DDPSTUB),y)
 NRD_DEPENDENCIES += stubs
@@ -15,7 +15,7 @@ endif
 
 NRD_INSTALL_STAGING = NO
 
-ifeq ($(BR2_ENABLE_DEBUG),y)
+ifeq ($(BR2_NRD_DEBUG_BUILD),y)
   NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-O0 -g -Wno-cast-align" -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g -Wno-cast-align"
 else
   NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align" -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align"
@@ -23,6 +23,7 @@ endif
 
 ifeq ($(BR2_NRD_GRAPHICS_DIRECTFB),y)
 NRD_GRAPHICS=directfb
+NRD_DEPENDENCIES += directfb
 else ifeq ($(BR2_NRD_GRAPHICS_GLES2),y)
 NRD_GRAPHICS=gles2
 else ifeq ($(BR2_NRD_GRAPHICS_GLES2_EGL),y)
@@ -33,14 +34,19 @@ endif
 
 NRD_INPUT=devinput
 
+NRD_CMAKE_FLAGS += -DGIBBON_NICE_THREADS=1
+NRD_CMAKE_FLAGS += -DGIBBON_PLATFORM=application-manager
+NRD_CMAKE_FLAGS += -DDPI_IMPLEMENTATION=skeleton
+NRD_CMAKE_FLAGS += -DBUILD_DPI_DIRECTORY=$(@D)/partner/dpi
+NRD_CMAKE_FLAGS += -DGIBBON_GRAPHICS=$(NRD_GRAPHICS)
+NRD_CMAKE_FLAGS += -DGIBBON_INPUT=$(NRD_INPUT)
+
 NRD_CONFIGURE_CMDS = 		\
 	mkdir $(@D)/output;	\
 	cd $(@D)/output;	\
 	$(TARGET_MAKE_ENV) BUILDROOT_TOOL_PREFIX="$(GNU_TARGET_NAME)-" cmake $(@D)/netflix	\
 		-DCMAKE_TOOLCHAIN_FILE=$(HOST_DIR)/usr/share/buildroot/toolchainfile.cmake	\
 		$(NRD_CMAKE_FLAGS)								\
-                -DGIBBON_GRAPHICS=$(NRD_GRAPHICS)						\
-		-DGIBBON_INPUT=$(NRD_INPUT)							\
 		-DSMALL_FLAGS:STRING="-s -O3" -DSMALL_CFLAGS:STRING="" -DSMALL_CXXFLAGS:STRING="-fvisibility=hidden -fvisibility-inlines-hidden" -DNRDAPP_TOOLS="manufSSgenerator" -DDPI_REFERENCE_DRM="none"
 
 NRD_BUILD_CMDS = cd $(@D)/output ; $(TARGET_MAKE_ENV) make 
@@ -50,6 +56,8 @@ define NRD_INSTALL_TARGET_CMDS
 	cp $(@D)/output/src/platform/gibbon/libWTF.so $(TARGET_DIR)/usr/lib
 	cp $(@D)/output/src/platform/gibbon/netflix $(TARGET_DIR)/usr/bin
 	cp $(@D)/output/src/platform/gibbon/manufss $(TARGET_DIR)/usr/bin
+	cp -R $(@D)/output/src/platform/gibbon/data $(TARGET_DIR)/var/lib/netflix
+
 endef
 
 $(eval $(cmake-package))
