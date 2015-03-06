a5e36e96cb87244ec0e6a64d5a802f4862a93ff4
Pierre Wielders <pierre@wielders.net>
2015-03-06 16:52:33 +0100

    Preparation to build the nrd application as a plufin. Options added to build the nrd app as a static or dynamic lib, whatever you prefer :-)

--- package/nrd/nrd.mk
+++ package/nrd/nrd.mk
@@ -16,31 +16,77 @@ endif
 
 NRD_INSTALL_STAGING = NO
 
-ifeq ($(BR2_NRD_DEBUG_BUILD),y)
-	NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-O0 -g -Wno-cast-align" -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g -Wno-cast-align"
-else
-	NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align" -DCMAKE_CXX_FLAGS_RELEASE="-O2 -DNDEBUG -Wno-cast-align"
-endif
-
 ifeq ($(BR2_NRD_GRAPHICS_DIRECTFB),y)
-NRD_GRAPHICS=directfb
+NRD_CMAKE_FLAGS += -DGIBBON_GRAPHICS=directfb
 NRD_DEPENDENCIES += alsa-lib portaudio webp ffmpeg tremor directfb
 else ifeq ($(BR2_NRD_GRAPHICS_GLES2),y)
-NRD_GRAPHICS=gles2
+NRD_CMAKE_FLAGS += -DGIBBON_GRAPHICS=gles2
 else ifeq ($(BR2_NRD_GRAPHICS_GLES2_EGL),y)
-NRD_GRAPHICS=gles2-egl
+NRD_CMAKE_FLAGS += -DGIBBON_GRAPHICS=gles2-egl
 else 
-NRD_GRAPHICS=null
+NRD_CMAKE_FLAGS += -DGIBBON_GRAPHICS=null
 endif
 
-NRD_INPUT=devinput
-
+ifeq ($(BR2_NRD_NICE_THREADS),y)
 NRD_CMAKE_FLAGS += -DGIBBON_NICE_THREADS=1
-NRD_CMAKE_FLAGS += -DGIBBON_PLATFORM=application-manager
+endif
+
+ifeq ($(BR2_PACKAGE_NRD_REF_SEKELETON), y)
 NRD_CMAKE_FLAGS += -DDPI_IMPLEMENTATION=skeleton
+else ifeq ($(BR2_PACKAGE_NRD_REF_X86), y)
+NRD_CMAKE_FLAGS += -DDPI_IMPLEMENTATION=reference
+else ifeq ($(BR2_PACKAGE_NRD_METRO), y)
+NRD_CMAKE_FLAGS += -DDPI_IMPLEMENTATION=metro
+endif
+
+ifeq ($(BR2_PACKAGE_NRD_APPLICATION), y)
+  NRD_CMAKE_FLAGS += -DGIBBON_MODE=executable
+  define NRD_TARGET_SET_DEFINITION
+	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/netflix $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/manufss $(TARGET_DIR)/usr/bin
+  endef
+else ifeq ($(BR2_PACKAGE_NRD_DYNAMICLIB), y)
+  NRD_RELOCATION_OPTION = -fPIC
+  NRD_INSTALL_STAGING = YES
+  NRD_CMAKE_FLAGS += -DGIBBON_MODE=shared
+  define NRD_TARGET_SET_DEFINITION
+	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/manufss $(TARGET_DIR)/usr/bin
+  endef
+  define NRD_INSTALL_STAGING_CMDS
+	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/libnetflix.a $(STAGING_DIR)/usr/lib
+	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/libnetflix.so $(STAGING_DIR)/usr/lib
+	mkdir -p $(STAGING_DIR)/usr/include/gibbon
+	cp -R $(@D)/output/nrdlib/include/nrd* $(STAGING_DIR)/usr/include/gibbon
+	cp -R $(@D)/output/src/platform/gibbon/include/gibbon/* $(STAGING_DIR)/usr/include/gibbon
+	cp -R $(@D)/netflix/src/platform/gibbon/*.h $(STAGING_DIR)/usr/include/gibbon
+	cp -R $(@D)/netflix/src/platform/gibbon/bridge/*.h $(STAGING_DIR)/usr/include/gibbon
+  endef
+else ifeq ($(BR2_PACKAGE_NRD_STATICLIB), y)
+  NRD_INSTALL_STAGING = YES
+  NRD_CMAKE_FLAGS += -DGIBBON_MODE=static
+  define NRD_TARGET_SET_DEFINITION
+	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/manufss $(TARGET_DIR)/usr/bin
+  endef
+  define NRD_INSTALL_STAGING_CMDS
+	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/libnetflix.a $(STAGING_DIR)/usr/lib
+	mkdir -p $(STAGING_DIR)/usr/include/gibbon
+	cp -R $(@D)/output/nrdlib/include/nrd* $(STAGING_DIR)/usr/include/gibbon
+	cp -R $(@D)/output/src/platform/gibbon/include/gibbon/* $(STAGING_DIR)/usr/include/gibbon
+	cp -R $(@D)/netflix/src/platform/gibbon/*.h $(STAGING_DIR)/usr/include/gibbon
+	cp -R $(@D)/netflix/src/platform/gibbon/bridge/*.h $(STAGING_DIR)/usr/include/gibbon
+  endef
+endif
+
+ifeq ($(BR2_NRD_DEBUG_BUILD),y)
+	NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="$(NRD_RELOCATION_OPTION) -O0 -g -Wno-cast-align" -DCMAKE_CXX_FLAGS_DEBUG="$(NRD_RELOCATION_OPTION) -O0 -g -Wno-cast-align"
+else
+	NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="$(NRD_RELOCATION_OPTION) -O2 -DNDEBUG -Wno-cast-align" -DCMAKE_CXX_FLAGS_RELEASE="$(NRD_RELOCATION_OPTION) -O2 -DNDEBUG -Wno-cast-align"
+endif
+
+#NRD_CMAKE_FLAGS += -DGIBBON_PLATFORM=application-manager
+NRD_CMAKE_FLAGS += -DGIBBON_PLATFORM=posix
 NRD_CMAKE_FLAGS += -DBUILD_DPI_DIRECTORY=$(@D)/partner/dpi
-NRD_CMAKE_FLAGS += -DGIBBON_GRAPHICS=$(NRD_GRAPHICS)
-NRD_CMAKE_FLAGS += -DGIBBON_INPUT=$(NRD_INPUT)
+NRD_CMAKE_FLAGS += -DGIBBON_INPUT=devinput
 
 NRD_CONFIGURE_CMDS = \
 	mkdir $(@D)/output;	\
@@ -55,9 +101,8 @@ NRD_BUILD_CMDS = cd $(@D)/output ; $(TARGET_MAKE_ENV) make
 define NRD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/libJavaScriptCore.so $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/libWTF.so $(TARGET_DIR)/usr/lib
-	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/netflix $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/manufss $(TARGET_DIR)/usr/bin
 	cp -R $(@D)/output/src/platform/gibbon/data $(TARGET_DIR)/var/lib/netflix
+	$(NRD_TARGET_SET_DEFINITION)
 endef
 
 $(eval $(cmake-package))
