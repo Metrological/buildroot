e784a832b76aaefb1612f1f7eca21fa58f63d081
albertd <albert@dahan.nl>
2015-03-06 19:36:54 +0100

    nrd: cleaning up config and mk file

--- package/nrd/nrd.mk
+++ package/nrd/nrd.mk
@@ -31,28 +31,28 @@ ifeq ($(BR2_NRD_NICE_THREADS),y)
 NRD_CMAKE_FLAGS += -DGIBBON_NICE_THREADS=1
 endif
 
-ifeq ($(BR2_PACKAGE_NRD_REF_SEKELETON), y)
+ifeq ($(BR2_PACKAGE_NRD_REF_SKELETON), y)
 NRD_CMAKE_FLAGS += -DDPI_IMPLEMENTATION=skeleton
 else ifeq ($(BR2_PACKAGE_NRD_REF_X86), y)
 NRD_CMAKE_FLAGS += -DDPI_IMPLEMENTATION=reference
-else ifeq ($(BR2_PACKAGE_NRD_METRO), y)
-NRD_CMAKE_FLAGS += -DDPI_IMPLEMENTATION=metro
+else ifeq ($(BR2_PACKAGE_NRD_REF_ML), y)
+NRD_CMAKE_FLAGS += -DDPI_IMPLEMENTATION=metrological
 endif
 
 ifeq ($(BR2_PACKAGE_NRD_APPLICATION), y)
-  NRD_CMAKE_FLAGS += -DGIBBON_MODE=executable
-  define NRD_TARGET_SET_DEFINITION
+NRD_CMAKE_FLAGS += -DGIBBON_MODE=executable
+define NRD_TARGET_SET_DEFINITION
 	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/netflix $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/manufss $(TARGET_DIR)/usr/bin
-  endef
+endef
 else ifeq ($(BR2_PACKAGE_NRD_DYNAMICLIB), y)
-  NRD_RELOCATION_OPTION = -fPIC
-  NRD_INSTALL_STAGING = YES
-  NRD_CMAKE_FLAGS += -DGIBBON_MODE=shared
-  define NRD_TARGET_SET_DEFINITION
+NRD_RELOCATION_OPTION = -fPIC
+NRD_INSTALL_STAGING = YES
+NRD_CMAKE_FLAGS += -DGIBBON_MODE=shared
+define NRD_TARGET_SET_DEFINITION
 	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/manufss $(TARGET_DIR)/usr/bin
-  endef
-  define NRD_INSTALL_STAGING_CMDS
+endef
+define NRD_INSTALL_STAGING_CMDS
 	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/libnetflix.a $(STAGING_DIR)/usr/lib
 	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/libnetflix.so $(STAGING_DIR)/usr/lib
 	mkdir -p $(STAGING_DIR)/usr/include/gibbon
@@ -60,27 +60,27 @@ else ifeq ($(BR2_PACKAGE_NRD_DYNAMICLIB), y)
 	cp -R $(@D)/output/src/platform/gibbon/include/gibbon/* $(STAGING_DIR)/usr/include/gibbon
 	cp -R $(@D)/netflix/src/platform/gibbon/*.h $(STAGING_DIR)/usr/include/gibbon
 	cp -R $(@D)/netflix/src/platform/gibbon/bridge/*.h $(STAGING_DIR)/usr/include/gibbon
-  endef
+endef
 else ifeq ($(BR2_PACKAGE_NRD_STATICLIB), y)
-  NRD_INSTALL_STAGING = YES
-  NRD_CMAKE_FLAGS += -DGIBBON_MODE=static
-  define NRD_TARGET_SET_DEFINITION
+NRD_INSTALL_STAGING = YES
+NRD_CMAKE_FLAGS += -DGIBBON_MODE=static
+define NRD_TARGET_SET_DEFINITION
 	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/manufss $(TARGET_DIR)/usr/bin
-  endef
-  define NRD_INSTALL_STAGING_CMDS
+endef
+define NRD_INSTALL_STAGING_CMDS
 	$(INSTALL) -m 755 $(@D)/output/src/platform/gibbon/libnetflix.a $(STAGING_DIR)/usr/lib
 	mkdir -p $(STAGING_DIR)/usr/include/gibbon
 	cp -R $(@D)/output/nrdlib/include/nrd* $(STAGING_DIR)/usr/include/gibbon
 	cp -R $(@D)/output/src/platform/gibbon/include/gibbon/* $(STAGING_DIR)/usr/include/gibbon
 	cp -R $(@D)/netflix/src/platform/gibbon/*.h $(STAGING_DIR)/usr/include/gibbon
 	cp -R $(@D)/netflix/src/platform/gibbon/bridge/*.h $(STAGING_DIR)/usr/include/gibbon
-  endef
+endef
 endif
 
 ifeq ($(BR2_NRD_DEBUG_BUILD),y)
-	NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="$(NRD_RELOCATION_OPTION) -O0 -g -Wno-cast-align" -DCMAKE_CXX_FLAGS_DEBUG="$(NRD_RELOCATION_OPTION) -O0 -g -Wno-cast-align"
+NRD_CMAKE_FLAGS += -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="$(NRD_RELOCATION_OPTION) -O0 -g -Wno-cast-align" -DCMAKE_CXX_FLAGS_DEBUG="$(NRD_RELOCATION_OPTION) -O0 -g -Wno-cast-align"
 else
-	NRD_CMAKE_FLAGS= -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="$(NRD_RELOCATION_OPTION) -O2 -DNDEBUG -Wno-cast-align" -DCMAKE_CXX_FLAGS_RELEASE="$(NRD_RELOCATION_OPTION) -O2 -DNDEBUG -Wno-cast-align"
+NRD_CMAKE_FLAGS += -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS_RELEASE="$(NRD_RELOCATION_OPTION) -O2 -DNDEBUG -Wno-cast-align" -DCMAKE_CXX_FLAGS_RELEASE="$(NRD_RELOCATION_OPTION) -O2 -DNDEBUG -Wno-cast-align"
 endif
 
 #NRD_CMAKE_FLAGS += -DGIBBON_PLATFORM=application-manager
