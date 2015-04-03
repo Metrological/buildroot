23a21eded65ddf4be6100b7382a9bdbd9b475226
Philippe Normand <philn@igalia.com>
2015-04-03 17:09:29 +0200

    libsrtp: bump to 1.5.2

--- package/libsrtp/libsrtp.mk
+++ package/libsrtp/libsrtp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSRTP_VERSION = 2b48665ac51be045e3fe87fbe35d38a3e93e8787
+LIBSRTP_VERSION = v1.5.2
 LIBSRTP_SITE = $(call github,cisco,libsrtp,$(LIBSRTP_VERSION))
 LIBSRTP_INSTALL_STAGING = YES
 LIBSRTP_LICENSE = BSD
