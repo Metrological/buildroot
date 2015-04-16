d5661917c90e5d73faa6d9a09b90489a3ba6c3ae
Philippe Normand <philn@igalia.com>
2015-04-16 18:58:19 +0200

    wpe.sh: request dummy gst-gl window

--- package/wpe/wpe.sh
+++ package/wpe/wpe.sh
@@ -5,5 +5,9 @@ chmod 0700 $XDG_RUNTIME_DIR
 
 export ATHOL_SHELL=/usr/lib/libWPEAtholShell.so
 
+# FIXME: gst-gl's dispmanx backend is messing up with our compositor
+# when it creates its initial 16x16px surface.
+export GST_GL_WINDOW=dummy
+
 # The PREFIX env var can be useful when debugging athol with gdbserver.
 WPE_SHELL_URL=$1 $PREFIX athol
