c0d3403505b6c0c4038188baacb36025b5cc9d67
Miguel Gomez <magomez@igalia.com>
2015-05-29 10:46:38 +0200

    version packages set

--- package/lttng-libust/lttng-libust.mk
+++ package/lttng-libust/lttng-libust.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LTTNG_LIBUST_SITE    = http://lttng.org/files/lttng-ust/
-LTTNG_LIBUST_VERSION = 2.2.0
+LTTNG_LIBUST_VERSION = 2.6.0
 LTTNG_LIBUST_SOURCE  = lttng-ust-$(LTTNG_LIBUST_VERSION).tar.bz2
 LTTNG_LIBUST_LICENSE = LGPLv2.1; GPLv2 for lttng-gen-tp and ust-ctl
 LTTNG_LIBUST_LICENSE_FILES = COPYING
--- package/lttng-tools/lttng-tools.mk
+++ package/lttng-tools/lttng-tools.mk
@@ -4,18 +4,19 @@
 #
 ################################################################################
 
-LTTNG_TOOLS_VERSION = 2.2.0
+LTTNG_TOOLS_VERSION = 2.6.0
 LTTNG_TOOLS_SITE    = http://lttng.org/files/lttng-tools/
 LTTNG_TOOLS_SOURCE  = lttng-tools-$(LTTNG_TOOLS_VERSION).tar.bz2
 LTTNG_TOOLS_LICENSE = GPLv2; LGPLv2.1 for include/lttng/* and src/lib/lttng-ctl/*
 # gpl-2.0.txt and lgpl-2.1.txt files are missing from the sources
 LTTNG_TOOLS_LICENSE_FILES = LICENSE
+LTTNG_TOOLS_LIBTOOL_PATCH = NO
 
 # The host-lttng-babeltrace technically isn't a required build
 # dependency. However, having the babeltrace utilities built for the
 # host is very useful, since those tools allow to convert the binary
 # trace format into an human readable format.
-LTTNG_TOOLS_DEPENDENCIES = liburcu popt host-lttng-babeltrace util-linux
+LTTNG_TOOLS_DEPENDENCIES = liburcu popt host-lttng-babeltrace util-linux libxml2
 
 LTTNG_AUTORECONF = YES
 
