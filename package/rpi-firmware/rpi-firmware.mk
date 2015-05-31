c550bbddf564a8d9400652bccf3fc152aebadc78
albertd <albert@dahan.nl>
2015-05-31 20:03:20 +0200

    rpi: bump linux, firmware and userland

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 70b05985cf2b2b893bc6e55b12794f32857a78dd
+RPI_FIRMWARE_VERSION = 2598325b534cba4b5f88959714b6c8f279357ef3
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
