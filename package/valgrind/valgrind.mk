2664c72b381c8bda445f0de9eee0bd962b1444b9
msieben <github@desiderium.eu>
2015-06-05 12:04:57 +0200

    valgrind: bump version and relax arm build constraint
    Based on http://git.buildroot.net/buildroot/tree/package/valgrind?id=fd0e5f6b8818b4301e70dcd24ac790639e3e2470.

--- package/valgrind/valgrind.mk
+++ package/valgrind/valgrind.mk
@@ -4,25 +4,21 @@
 #
 ################################################################################
 
-VALGRIND_VERSION = 3.9.0
-VALGRIND_SITE    = http://valgrind.org/downloads/
-VALGRIND_SOURCE  = valgrind-$(VALGRIND_VERSION).tar.bz2
+VALGRIND_VERSION = 3.10.1
+VALGRIND_SITE = http://valgrind.org/downloads
+VALGRIND_SOURCE = valgrind-$(VALGRIND_VERSION).tar.bz2
 VALGRIND_LICENSE = GPLv2 GFDLv1.2
 VALGRIND_LICENSE_FILES = COPYING COPYING.DOCS
-VALGRIND_CONF_OPT = --disable-tls
+VALGRIND_AUTORECONF = YES
 
-# On ARM, Valgrind only supports ARMv7, and uses the arch part of the
-# host tuple to determine whether it's being built for ARMv7 or
-# not. Therefore, we adjust the host tuple to specify we're on
-# ARMv7. The valgrind package is guaranteed, through Config.in, to
-# only be selected on Cortex A8 and Cortex A9 platforms.
-ifeq ($(BR2_cortex_a8)$(BR2_cortex_a9),y)
-VALGRIND_CONF_OPT += \
-	--host=$(patsubst arm-%,armv7-%,$(GNU_TARGET_NAME))
-endif
+VALGRIND_CONF_OPTS += \
+		--disable-tls \
+                --target=$(GNU_TARGET_NAME) \
+                --host=$(GNU_TARGET_NAME) \
+                --build=$(GNU_HOST_NAME)
 
 define VALGRIND_INSTALL_UCLIBC_SUPP
-	install -D -m 0644 package/valgrind/uclibc.supp $(TARGET_DIR)/usr/lib/valgrind/uclibc.supp
+	$(INSTALL) -D -m 0644 package/valgrind/uclibc.supp $(TARGET_DIR)/usr/lib/valgrind/uclibc.supp
 endef
 
 VALGRIND_POST_INSTALL_TARGET_HOOKS += VALGRIND_INSTALL_UCLIBC_SUPP
@@ -90,12 +86,12 @@ endef
 VALGRIND_POST_INSTALL_TARGET_HOOKS += VALGRIND_REMOVE_DHAT
 endif
 
-ifeq ($(BR2_PACKAGE_VALGRIND_PTRCHECK),)
-define VALGRIND_REMOVE_PTRCHECK
-	rm -f $(TARGET_DIR)/usr/lib/valgrind/*ptrcheck*
+ifeq ($(BR2_PACKAGE_VALGRIND_SGCHECK),)
+define VALGRIND_REMOVE_SGCHECK
+	rm -f $(TARGET_DIR)/usr/lib/valgrind/*sgcheck*
 endef
 
-VALGRIND_POST_INSTALL_TARGET_HOOKS += VALGRIND_REMOVE_PTRCHECK
+VALGRIND_POST_INSTALL_TARGET_HOOKS += VALGRIND_REMOVE_SGCHECK
 endif
 
 ifeq ($(BR2_PACKAGE_VALGRIND_BBV),)
@@ -121,5 +117,16 @@ endef
 
 VALGRIND_POST_INSTALL_TARGET_HOOKS += VALGRIND_REMOVE_NULGRIND
 endif
+
+define VALGRIND_CONFIGURE_CMDS
+	(cd $(@D); \
+	$(TARGET_CONFIGURE_OPTS) \
+	./configure \
+	CFLAGS="$(TARGET_CFLAGS)" \
+	CPPFLAGS="$(TARGET_CPPFLAGS)" \
+	CXXFLAGS="$(TARGET_CXXFLAGS)" \
+	$(VALGRIND_CONF_OPTS) \
+	)
+endef
 
 $(eval $(autotools-package))
