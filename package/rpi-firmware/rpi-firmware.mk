1ce226408498d7f74dd4f6716512737da77dce9b
albertd <albert@dahan.nl>
2015-06-14 11:58:10 +0200

    rpi: bump linux and firmware

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 5c1848771795600170250a0ea1ff4a509e20859e
+RPI_FIRMWARE_VERSION = 37600d5d2605bc553552b0409886735b5f4b6bdd
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
