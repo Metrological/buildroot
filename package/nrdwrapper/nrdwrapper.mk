21250d55065ef60fa45693dce1e38f2854d0feb7
msieben <github@desiderium.eu>
2015-07-09 10:37:41 +0200

    nrdwrapper: initial commit

---
+++ package/nrdwrapper/nrdwrapper.mk
@@ -0,0 +1,51 @@
+################################################################################
+#
+# nrdwrapper
+#
+################################################################################
+
+NRDWRAPPER_VERSION = custom
+NRDWRAPPER_SITE = /home/debian/nrdwrapper/
+NRDWRAPPER_SITE_METHOD = local
+NRDWRAPPER_LICENSE = PROPRIETARY
+
+NRDWRAPPER_DEPENDENCIES = cppsdk
+
+NRDWRAPPER_INSTALL_STAGING = YES
+
+NRDWRAPPER_CXXFLAGS = \
+	$(TARGET_CXXFLAGS) -Wall -v 
+
+ifeq ($(BR2_ENABLE_DEBUG),y)
+# enable ASSERT
+NRDWRAPPER_CXXFLAGS += -D__DEBUG__
+endif
+
+ifneq ($(BR2_PACKAGE_CPPSDK),y)
+$(error CPPSDK is required. Check BR2_PACKAGE_CPPSDK.)
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_OPENGL_EGL),y)
+NRDWRAPPER_DEPENDENCIES   += $(call qstrip,$(BR2_PACKAGE_PROVIDES_OPENGL_EGL))
+NRDWRAPPER_EXTRA_CFLAGS   += $(shell $(PKG_CONFIG_HOST_BINARY) --cflags egl)
+NRDWRAPPER_EXTRA_CXXFLAGS += $(shell $(PKG_CONFIG_HOST_BINARY) --cflags  egl)
+NRDWRAPPER_EXTRA_LDFLAGS  += $(shell $(PKG_CONFIG_HOST_BINARY) --libs egl)
+else
+$(error EGL is required. Check BR2_PACKAGE_NRD_GRAPHICS_GLES2_EGL.)
+endif
+
+define NRDWRAPPER_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) CXX="$(TARGET_CXX)" CPPFLAGS="$(TARGET_CPPFLAGS)" CXXFLAGS="$(NRDWRAPPER_CXXFLAGS) $(NRDWRAPPER_EXTRA_CXXFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" -C $(@D)/
+endef
+
+define NRDWRAPPER_INSTALL_STAGING_CMDS
+	mkdir -p $(STAGING_DIR)/usr/lib/
+	$(INSTALL) -m 755 $(@D)/libnrdwrapper.{a,so} $(STAGING_DIR)/usr/lib/
+endef
+
+define NRDWRAPPER_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -m 755 $(@D)/libnrdwrapper.so $(TARGET_DIR)/usr/lib/
+endef
+
+$(eval $(generic-package))
