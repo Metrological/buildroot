bb1dcb3590f5278a787b693379cf880ff387d1c0
albertd <albert@dahan.nl>
2015-07-13 23:42:22 +0200

    qt5webkit: bump version

--- package/qt5/qt5webkit/qt5webkit.mk
+++ package/qt5/qt5webkit/qt5webkit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-QT5WEBKIT_VERSION = 41ef90534f63eb94d93b6d0d5b95bc55c9c50666
+QT5WEBKIT_VERSION = f0888c516bc5ef6a0f3db00aa1288000acd060d9
 ifeq ($(BR2_QT5WEBKIT_USE_WEBRTC),y)
 QT5WEBKIT_VERSION = de07f58fb904c81794af37238e2c0c2989a59898
 endif
