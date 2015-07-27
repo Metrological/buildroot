fa28003a7b20ccbca962f35dd08f6164d2453cca
Pierre Wielders <pierre@wielders.net>
2015-07-27 19:29:16 +0200

    If the Broadcom reference package is selected, Athol will use of for display handling.

--- package/athol/athol.mk
+++ package/athol/athol.mk
@@ -8,8 +8,13 @@ ATHOL_VERSION = 7dbbc0e6e0654b8350609959971b32793b25253e
 ATHOL_SITE = $(call github,Metrological,athol,$(ATHOL_VERSION))
 
 ATHOL_INSTALL_STAGING = YES
-ATHOL_DEPENDENCIES = wayland libegl libglib2 libinput
 
+ifeq ($(BR2_PACKAGE_BCM_REFSW),y)
+ATHOL_DEPENDENCIES = wayland libinput libglib2
+ATHOL_CONF_OPT = -DCMAKE_BUILD_TYPE=Release -DBROADCOM_NEXUS=1
+else
+ATHOL_DEPENDENCIES = wayland libegl libglib2 libinput
 ATHOL_CONF_OPT = -DCMAKE_BUILD_TYPE=Release
+endif
 
 $(eval $(cmake-package))
