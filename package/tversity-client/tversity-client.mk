373ee9b09c6519f293556ec35e0bea0ef23dc88f
brainstem123 <c.custers@metrological.com>
2015-07-28 17:45:24 +0200

    Fix indentation configuration files.

--- package/tversity-client/tversity-client.mk
+++ package/tversity-client/tversity-client.mk
@@ -11,23 +11,23 @@ TVERSITY_CLIENT_LICENSE = PROPRIETARY
 
 TVERSITY_CLIENT_DEPENDENCIES = host-pkgconf libgles libegl \
 	libsoup libopenmax \
-  gstreamer1 \
+	gstreamer1 \
 	gst1-plugins-base \
-  gst1-plugins-good \
-  gst1-plugins-bad \
+	gst1-plugins-good \
+	gst1-plugins-bad \
 	gst1-plugins-ugly 
 
 ifeq ($(BR2_ENABLE_DEBUG),y)
 BUILDTYPE=Debug
 FLAGS= -DCMAKE_C_FLAGS_DEBUG="-O0 -g -DCLOUD_CLIENT_DEBUG" \
- -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g -DCLOUD_CLIENT_DEBUG"
+	-DCMAKE_CXX_FLAGS_DEBUG="-O0 -g -DCLOUD_CLIENT_DEBUG"
 else
 BUILDTYPE=Release
 FLAGS= -DCMAKE_C_FLAGS_RELEASE="-O2" \
- -DCMAKE_CXX_FLAGS_RELEASE="-O2"
+	-DCMAKE_CXX_FLAGS_RELEASE="-O2"
 endif
 
 TVERSITY_CLIENT_CONF_OPT = -DCMAKE_BUILD_TYPE=$(BUILDTYPE) \
- $(FLAGS) \
+	$(FLAGS) \
 
 $(eval $(cmake-package))
