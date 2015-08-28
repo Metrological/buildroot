03e297a618f44f97ca43004885d2cf9f3a4fdc10
Philippe Normand <philn@igalia.com>
2015-08-28 10:13:52 +0200

    gdb: 7.9.x versions support

--- package/gdb/gdb.mk
+++ package/gdb/gdb.mk
@@ -23,7 +23,7 @@ ifeq ($(GDB_VERSION),6.7.1-avr32-2.1.5)
 GDB_SITE = ftp://www.at91.com/pub/buildroot/
 endif
 
-GDB_SOURCE ?= gdb-$(GDB_VERSION).tar.bz2
+GDB_SOURCE ?= gdb-$(GDB_VERSION).tar.gz
 GDB_LICENSE = GPLv2+ LGPLv2+ GPLv3+ LGPLv3+
 GDB_LICENSE_FILES = COPYING COPYING.LIB COPYING3 COPYING3.LIB
 
