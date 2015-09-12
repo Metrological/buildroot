6ea495549ccca2ae172f309256ed9624ec444e66
albertd <albert@dahan.nl>
2015-09-12 14:05:53 +0200

    greenpeak: add userspace application

--- package/greenpeak/greenpeak.mk
+++ package/greenpeak/greenpeak.mk
@@ -11,12 +11,29 @@ GREENPEAK_DEPENDENCIES = linux
 
 GREENPEAK_CHIP = $(call qstrip,$(BR2_PACKAGE_GREENPEAK_TYPE))
 
-define GREENPEAK_BUILD_CMDS
+GREENPEAK_EXTRA_CFLAGS = \
+	-std=gnu99 \
+	-fomit-frame-pointer \
+	-fno-strict-aliasing \
+	-fno-pic \
+	-ffreestanding
+
+define GREENPEAK_BUILD_MODULE
 	GP_CHIP=$(GREENPEAK_CHIP) $(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M=$(@D)/driver modules
 endef
 
-define GREENPEAK_INSTALL_TARGET_CMDS
+define GREENPEAK_INSTALL_MODULE
 	GP_CHIP=$(GREENPEAK_CHIP) $(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M=$(@D)/driver modules_install
+endef
+
+define GREENPEAK_BUILD_CMDS
+	COMPILER=buildroot $(TARGET_MAKE_ENV) $(MAKE1) CC="$(TARGET_CC)" CFLAGS_COMPILER="$(TARGET_CFLAGS) $(GREENPEAK_EXTRA_CFLAGS)" -C $(@D)/ZRCTarget_GP501_RPi
+	$(GREENPEAK_BUILD_MODULE)
+endef
+
+define GREENPEAK_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/ZRCTarget_GP501_RPi/Work/ZRCTarget_GP501_RPi.elf $(TARGET_DIR)/usr/bin/zrcd
+	$(GREENPEAK_INSTALL_MODULE)
 endef
 
 $(eval $(generic-package))
