6a6e98d981782eb27cc99bb6e2d7db935c9cd1a6
msieben <github@desiderium.eu>
2015-10-15 16:29:23 +0200

    dxdrm: sync with master
    See https://github.com/Metrological/buildroot/tree/master/package/dxdrm.

--- package/dxdrm/dxdrm.mk
+++ package/dxdrm/dxdrm.mk
@@ -4,29 +4,51 @@
 #
 ################################################################################
 
-DXDRM_VERSION = 1.6.0
-DXDRM_SITE = ../dxdrm
-DXDRM_SITE_METHOD = local
+DXDRM_VERSION = 474837c846bfea260ff3f7b170cac305a7ad5e71
+DXDRM_SITE_METHOD = git
+DXDRM_SITE = git@github.com:Metrological/dxdrm.git
 DXDRM_LICENSE = PROPRIETARY
 DXDRM_REDISTRIBUTE = NO
 
 DXDRM_INSTALL_STAGING = YES
 
-DXDRM_DEPENDENCIES = libcurl
+DXDRM_DEPENDENCIES += libcurl
+
+ifeq ($(BR2_PACKAGE_DXDRM_INTERNAL), y)
+DXDRM_LOCATOR = internal
+else
+DXDRM_LOCATOR = external
+DXDRM_DEPENDENCIES += openssl
+DXDRM_DEPENDENCIES += cppsdk
+endif
 
 define DXDRM_INSTALL_STAGING_CMDS
-	$(INSTALL) -m 755 $(@D)/$(call qstrip,$(BR2_ARCH))/libDxDrm.so $(STAGING_DIR)/usr/lib/libDxDrm.so
-	$(INSTALL) -m 755 $(@D)/$(call qstrip,$(BR2_ARCH))/libTrusted.so $(STAGING_DIR)/usr/lib/libTrusted.so
+	$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libDxDrm.so $(STAGING_DIR)/usr/lib/libDxDrm.so
+	$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libTrusted.so $(STAGING_DIR)/usr/lib/libTrusted.so
 	$(INSTALL) -d -m 755 $(STAGING_DIR)/usr/include/dxdrm
-	$(INSTALL) -m 644 $(@D)/include/*.h $(STAGING_DIR)/usr/include/dxdrm
+	$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/include/*.h $(STAGING_DIR)/usr/include/dxdrm
+	if [ "x$(BR2_PACKAGE_DXDRM_EXTERNAL)" = "xy" ] ; then \
+		$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libprovision.so $(STAGING_DIR)/usr/lib/libprovision.so; \
+		$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libprovisionproxy.so $(STAGING_DIR)/usr/lib/libprovisionproxy.so; \
+		$(INSTALL) -d -m 755 $(STAGING_DIR)/usr/include/rpc; \
+		$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/include/rpc/*.h $(STAGING_DIR)/usr/include/rpc; \
+		$(INSTALL) -d -m 755 $(STAGING_DIR)/usr/include/provision; \
+		$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/include/provision/*.h $(STAGING_DIR)/usr/include/provision; \
+	fi
 endef
 
 define DXDRM_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 $(@D)/$(call qstrip,$(BR2_ARCH))/libDxDrm.so $(TARGET_DIR)/usr/lib/libDxDrm.so
-	$(INSTALL) -m 755 $(@D)/$(call qstrip,$(BR2_ARCH))/libTrusted.so $(TARGET_DIR)/usr/lib/libTrusted.so
+	$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libDxDrm.so $(TARGET_DIR)/usr/lib/libDxDrm.so
+	$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libTrusted.so $(TARGET_DIR)/usr/lib/libTrusted.so
 	$(INSTALL) -d -m 755 $(TARGET_DIR)/etc/dxdrm
-	$(INSTALL) -m 644 $(@D)/dxdrm.config $(TARGET_DIR)/etc/dxdrm
-	$(INSTALL) -m 644 $(@D)/credentials/* $(TARGET_DIR)/etc/dxdrm
+	$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/dxdrm.config $(TARGET_DIR)/etc/dxdrm
+	if [ "x$(BR2_PACKAGE_DXDRM_INTERNAL)" = "xy" ]; then \
+		$(INSTALL) -m 644 $(@D)/$(DXDRM_LOCATOR)/credentials/* $(TARGET_DIR)/etc/dxdrm; \
+	fi
+	if [ "x$(BR2_PACKAGE_DXDRM_EXTERNAL)" = "xy" ]; then \
+		$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libprovision.so $(TARGET_DIR)/usr/lib/libprovision.so; \
+		$(INSTALL) -m 755 $(@D)/$(DXDRM_LOCATOR)/$(call qstrip,$(BR2_ARCH))/release/libprovisionproxy.so $(TARGET_DIR)/usr/lib/libprovisionproxy.so; \
+	fi
 endef
 
 $(eval $(generic-package))
