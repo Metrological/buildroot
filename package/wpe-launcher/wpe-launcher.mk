4401eba42fa60b56362f42e4ca177ea9745ee03b
albertd <albert@dahan.nl>
2015-10-16 22:08:01 +0200

    [wpe-launcher] bump version

--- package/wpe-launcher/wpe-launcher.mk
+++ package/wpe-launcher/wpe-launcher.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPE_LAUNCHER_VERSION = 38ee77cceb4534762914f169956f6cb977f71509
+WPE_LAUNCHER_VERSION = 77d057cb0345b0cf7118d4f1b492798251a78c21
 WPE_LAUNCHER_SITE = $(call github,Metrological,wpe-launcher,$(WPE_LAUNCHER_VERSION))
 
 WPE_LAUNCHER_DEPENDENCIES = wpe rpi-userland
