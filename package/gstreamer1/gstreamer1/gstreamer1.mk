7d185804f5c0ce6d4323b2dc0e548fe6d94b9b99
albertd <albert@dahan.nl>
2015-10-29 11:02:32 +0100

    [gstreamer1] bump git versions

--- package/gstreamer1/gstreamer1/gstreamer1.mk
+++ package/gstreamer1/gstreamer1/gstreamer1.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifeq ($(BR2_PACKAGE_GSTREAMER1_GIT),y)
-GSTREAMER1_VERSION = ebd2748cd0d6b2e0b35c1d87b7170dfe673ec917
+GSTREAMER1_VERSION = c47e49e1e3f2697b0611d6919f53d4a8b2399043
 endif
 GSTREAMER1_SOURCE = gstreamer-$(GSTREAMER1_VERSION).tar.gz
 GSTREAMER1_SITE = http://cgit.freedesktop.org/gstreamer/gstreamer/snapshot/
