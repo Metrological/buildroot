1fa548ddeb209c454bbd4673d9a0339329c2a3fd
albertd <albert@dahan.nl>
2015-10-30 11:55:32 +0100

    [eos] adding initial version for wpe

--- package/athol/athol.mk
+++ package/athol/athol.mk
@@ -6,14 +6,13 @@
 
 ATHOL_VERSION = 7dbbc0e6e0654b8350609959971b32793b25253e
 ATHOL_SITE = $(call github,Metrological,athol,$(ATHOL_VERSION))
+ATHOL_DEPENDENCIES = wayland wpe
 
 ATHOL_INSTALL_STAGING = YES
 
 ifeq ($(BR2_PACKAGE_BCM_REFSW),y)
-ATHOL_DEPENDENCIES = wayland libegl libglib2 libinput bcm-refsw
 ATHOL_CONF_OPT = -DCMAKE_BUILD_TYPE=Release -DBROADCOM_NEXUS=1
 else
-ATHOL_DEPENDENCIES = wayland libegl libglib2 libinput
 ATHOL_CONF_OPT = -DCMAKE_BUILD_TYPE=Release
 endif
 
