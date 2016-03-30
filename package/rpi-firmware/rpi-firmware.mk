fcfd2ee3519ecc84b46509b6618ad9dc8ac464ce
wouterlucas <wouter@wouterlucas.com>
2016-03-30 12:05:56 -0700

    [rpi] bump rpi firmware

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,53 +4,72 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 4047fe26797884cedf53bc8671d19e7f6f9f59d5
+RPI_FIRMWARE_VERSION = 4bf906cdd221c4f6815d0da7dda0cd59d25d945b
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
+RPI_FIRMWARE_INSTALL_IMAGES = YES
 
-ifeq ($(BR2_TARGET_ROOTFS_INITRAMFS),y)
-RPI_FIRMWARE_CONF_DIR=boot
-else
-RPI_FIRMWARE_CONF_DIR=etc
-endif
+RPI_FIRMWARE_DEPENDENCIES += host-rpi-firmware
 
-ifeq ($(BR2_PACKAGE_RPI_FIRMWARE_WIFI),y)
-define RPI_FIRMWARE_INSTALL_TARGET_WIFI
-	grep -q 'wlan0' $(TARGET_DIR)/etc/network/interfaces || \
-		echo -e '\nauto wlan0\niface wlan0 inet dhcp\npre-up sleep 2 && wpa_supplicant -B w -D wext -i wlan0 -c /$(RPI_FIRMWARE_CONF_DIR)/wpa_supplicant.conf\npost-down killall -q wpa_supplicant' >> $(TARGET_DIR)/etc/network/interfaces
+ifeq ($(BR2_PACKAGE_RPI_FIRMWARE_INSTALL_DTBS),y)
+define RPI_FIRMWARE_INSTALL_DTB
+	$(INSTALL) -D -m 0644 $(@D)/boot/bcm2708-rpi-b.dtb $(BINARIES_DIR)/rpi-firmware/bcm2708-rpi-b.dtb
+	$(INSTALL) -D -m 0644 $(@D)/boot/bcm2708-rpi-b-plus.dtb $(BINARIES_DIR)/rpi-firmware/bcm2708-rpi-b-plus.dtb
+	$(INSTALL) -D -m 0644 $(@D)/boot/bcm2709-rpi-2-b.dtb $(BINARIES_DIR)/rpi-firmware/bcm2709-rpi-2-b.dtb
+	$(INSTALL) -D -m 0644 $(@D)/boot/bcm2710-rpi-3-b.dtb $(BINARIES_DIR)/rpi-firmware/bcm2710-rpi-3-b.dtb
 endef
 endif
 
-ifneq ($(BR2_TARGET_GENERIC_GETTY_PORT),)
-define RPI_FIRMWARE_UPDATE_TTY
-	sed -i s/tty1/$(call qstrip,$(BR2_TARGET_GENERIC_GETTY_PORT))/ \
-		$(BINARIES_DIR)/rpi-firmware/cmdline.txt
+ifeq ($(BR2_PACKAGE_RPI_FIRMWARE_INSTALL_DTB_OVERLAYS),y)
+define RPI_FIRMWARE_INSTALL_DTB_OVERLAYS
+	for ovldtb in  $(@D)/boot/overlays/*.dtb; do \
+		$(INSTALL) -D -m 0644 $${ovldtb} $(BINARIES_DIR)/rpi-firmware/overlays/$${ovldtb##*/} || exit 1; \
+	done
 endef
 endif
 
-ifeq ($(BR2_TARGET_ROOTFS_INITRAMFS),y)
-define RPI_FIRMWARE_USE_RAMDISK
+ifeq ($(BR2_PACKAGE_RPI_FIRMWARE_INSTALL_VCDBG),y)
+define RPI_FIRMWARE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0700 $(@D)/$(if BR2_ARM_EABIHF,hardfp/)opt/vc/bin/vcdbg \
+		$(TARGET_DIR)/usr/sbin/vcdbg
+endef
+endif # INSTALL_VCDBG
+
+define RPI_FIRMWARE_MOUNT_BOOT
+	mkdir -p $(TARGET_DIR)/boot
+	grep -q '^/dev/mmcblk0p1' $(TARGET_DIR)/etc/fstab || \
+		echo -e '/dev/mmcblk0p1 /boot vfat defaults 0 0' >> $(TARGET_DIR)/etc/fstab
+endef
+
+ifeq ($(BR2_TARGET_ROOTFS_CPIO),y)
+define RPI_FIRMWARE_MOUNT_ROOT
 	mkdir -p $(TARGET_DIR)/root
 	grep -q '^/dev/mmcblk0p2' $(TARGET_DIR)/etc/fstab || \
 		echo -e '/dev/mmcblk0p2 /root ext4 defaults 0 0' >> $(TARGET_DIR)/etc/fstab
 endef
 endif
 
-define RPI_FIRMWARE_INSTALL_TARGET_CMDS
+define RPI_FIRMWARE_INSTALL_IMAGES_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/boot/bootcode.bin $(BINARIES_DIR)/rpi-firmware/bootcode.bin
 	$(INSTALL) -D -m 0644 $(@D)/boot/start$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).elf $(BINARIES_DIR)/rpi-firmware/start.elf
 	$(INSTALL) -D -m 0644 $(@D)/boot/fixup$(BR2_PACKAGE_RPI_FIRMWARE_BOOT).dat $(BINARIES_DIR)/rpi-firmware/fixup.dat
 	$(INSTALL) -D -m 0644 package/rpi-firmware/config.txt $(BINARIES_DIR)/rpi-firmware/config.txt
 	$(INSTALL) -D -m 0644 package/rpi-firmware/cmdline.txt $(BINARIES_DIR)/rpi-firmware/cmdline.txt
-	$(RPI_FIRMWARE_UPDATE_TTY)
-	grep -q 'eth0' $(TARGET_DIR)/etc/network/interfaces || \
-		echo -e '\nauto eth0\niface eth0 inet dhcp\npre-up sleep 2' >> $(TARGET_DIR)/etc/network/interfaces
-	$(RPI_FIRMWARE_INSTALL_TARGET_WIFI)
-	mkdir -p $(TARGET_DIR)/boot
-	grep -q '^/dev/mmcblk0p1' $(TARGET_DIR)/etc/fstab || \
-		echo -e '/dev/mmcblk0p1 /boot vfat defaults 0 0' >> $(TARGET_DIR)/etc/fstab
-	$(RPI_FIRMWARE_USE_RAMDISK)
+	$(RPI_FIRMWARE_MOUNT_BOOT)
+	$(RPI_FIRMWARE_MOUNT_ROOT)
+	$(RPI_FIRMWARE_INSTALL_DTB)
+	$(RPI_FIRMWARE_INSTALL_DTB_OVERLAYS)
+endef
+
+# We have no host sources to get, since we already
+# bundle the script we want to install.
+HOST_RPI_FIRMWARE_SOURCE =
+HOST_RPI_FIRMWARE_DEPENDENCIES =
+
+define HOST_RPI_FIRMWARE_INSTALL_CMDS
+	$(INSTALL) -D -m 0755 package/rpi-firmware/mkknlimg $(HOST_DIR)/usr/bin/mkknlimg
 endef
 
 $(eval $(generic-package))
+$(eval $(host-generic-package))
