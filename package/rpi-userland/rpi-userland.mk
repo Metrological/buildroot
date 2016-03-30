4f01ab6826de293d3124aacd76cda4bd4dc78923
wouterlucas <wouter@wouterlucas.com>
2016-03-30 12:07:30 -0700

    [rpi] bump rpi userland

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 40e377862410371a9962db79b81fd4f0f266430a
+RPI_USERLAND_VERSION = 2f56a2943a9eb8420df52ccf91f5a1c5a70e8713
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
