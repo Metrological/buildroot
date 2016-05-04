390ccf42e3088c8076429caff717ed95a3fedfe6
wouterlucas <wouter@wouterlucas.com>
2016-05-04 10:17:29 -0700

    Aligning libpng version

--- package/libpng/libpng.mk
+++ package/libpng/libpng.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBPNG_VERSION = 1.6.8
-LIBPNG_SERIES = 16
+LIBPNG_VERSION = 1.5.15
+LIBPNG_SERIES = 15
 LIBPNG_SOURCE = libpng-$(LIBPNG_VERSION).tar.xz
 LIBPNG_SITE = http://downloads.sourceforge.net/project/libpng/libpng${LIBPNG_SERIES}/older-releases/$(LIBPNG_VERSION)
 LIBPNG_LICENSE = libpng license
@@ -13,7 +13,7 @@ LIBPNG_LICENSE_FILES = LICENSE
 LIBPNG_INSTALL_STAGING = YES
 LIBPNG_DEPENDENCIES = host-pkgconf zlib
 LIBPNG_CONFIG_SCRIPTS = libpng$(LIBPNG_SERIES)-config libpng-config
-LIBPNG_CONF_OPT = $(if $(BR2_ARM_CPU_HAS_NEON),--enable-arm-neon=yes,--enable-arm-neon=no)
+LIBPNG_CONF_OPT = $(if $(BR2_ARM_CPU_HAS_NEON),--enable-arm-neon=on,--enable-arm-neon=off)
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
